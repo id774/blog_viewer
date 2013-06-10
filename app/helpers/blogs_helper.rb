7acdcf605978c4dbc0ff22bac912a0e7d81a578a
id774 <idnanashi@gmail.com>
2013-06-10 09:44:08 +0900

    29 Rinku.auto_link as default

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 module BlogsHelper
+  def auto_link(text)
+    raw Rinku.auto_link(h(text))
+  end
+
   def cutoff_by_char(str, max_length = 140, footer = "...")
     arr_str = str.split(//)
     if arr_str.length > max_length
